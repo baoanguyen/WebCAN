40ca2c5e29aa8c6dbc99f757dce324dfed01d8cf
Bao Anh Nguyen <ba2nguye@uwaterloo.ca>
2022-07-18 23:25:49 -0400

    Installed react and node

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=webcan
+FLASK_ENV=development
--- webcan/__init__.py
+++ webcan/__init__.py
@@ -13,8 +13,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from . import auth
-    app.register_blueprint(auth.bp)
+    # from . import auth
+    # app.register_blueprint(auth.bp)
 
     from . import upload
     from . import webcan
