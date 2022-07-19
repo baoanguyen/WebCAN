f9317785cf9ee193af01deb8d38116daace404a7
Bao Anh Nguyen <ba2nguye@uwaterloo.ca>
2022-07-18 23:54:26 -0400

    Debugging chartJS with React, no API

--- react-webcan/src/App.js
+++ react-webcan/src/App.js
@@ -1,7 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Line } from 'react-chartjs-2'
+import { useEffect } from 'react';
+
 
 function App() {
+  //ChartJS
+  //const labels = Utils.months({count: 7});
+  const labels = ['month1', 'month2']
+  const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+  };
+  const config = {
+    type: 'line',
+    data: data,
+  };
+
+  // //API
+  // useEffect(() => {
+  //   fetch('/line').then(res => res.json()).then(data => {
+      
+  //   })
+  // }, [])
+
+  //React Part
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +46,7 @@ function App() {
         >
           Learn React
         </a>
+        <Line data={data} />
       </header>
     </div>
   );
