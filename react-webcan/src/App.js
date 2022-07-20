17f383feff16ded9ff868554c45e3c3980da7ed0
Bao Anh Nguyen <ba2nguye@uwaterloo.ca>
2022-07-19 23:02:55 -0400

    Some working bar chart

--- react-webcan/src/App.js
+++ react-webcan/src/App.js
@@ -1,55 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import { Line } from 'react-chartjs-2'
-import { useEffect } from 'react';
+//import React, { useState } from "react/cjs/react.pro"
+import React, { useState } from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import {UserData} from './Data'
 
 
-function App() {
-  //ChartJS
-  //const labels = Utils.months({count: 7});
-  const labels = ['month1', 'month2']
-  const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  }]
-  };
-  const config = {
-    type: 'line',
-    data: data,
-  };
+function App(){
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [{
+      label: "Users gained",
+      data: UserData.map((data) => data.userGain),
+    }]
+  })
 
-  // //API
-  // useEffect(() => {
-  //   fetch('/line').then(res => res.json()).then(data => {
-      
-  //   })
-  // }, [])
-
-  //React Part
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Line data={data} />
-      </header>
-    </div>
-  );
+      <BarChart chartData={userData}/>
+    </div>);
 }
+
+
 
 export default App;
