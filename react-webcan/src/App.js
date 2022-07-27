cd985338a7fe2c080bcdcfb521b2fbc0000b9655
Bao Anh Nguyen <ba2nguye@uwaterloo.ca>
2022-07-27 01:23:33 -0400

    Added JSON exchange. Currently bugs rendering

--- react-webcan/src/App.js
+++ react-webcan/src/App.js
@@ -1,22 +1,55 @@
 //import React, { useState } from "react/cjs/react.pro"
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import BarChart from "./components/BarChart";
-import {UserData} from './Data'
+import LineChart from "./components/LineChart";
+//import {UserData} from './Data'
 
+import axios from 'axios'
 
 function App(){
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [{
-      label: "Users gained",
-      data: UserData.map((data) => data.userGain),
-    }]
-  })
+  //ChartJS Test
+  const [userData, setUserData] = useState({});
+
+  const chart = () => {
+    let dataA = [];
+    let dataB = [];
+    
+    axios
+      .get('http://127.0.0.1:5000/graph/getData')
+      .then(response => {
+        console.log(response)
+        for(const dataObj of response.data){
+          dataA.push(parseInt(dataObj.year))
+          dataB.push(parseInt(dataObj.userGain))
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+      console.log(dataA, dataB);
+    
+    setUserData({
+      labels: dataA,
+      datasets: [
+        {
+          label: "Users Gained",
+          data: dataB,
+          backgroundColor: ["rgba(75. 192. 192. 0.6)"],
+          borderWidth: 4
+        }
+      ],
+    });
+  };
+  
+
+  useEffect(() => {
+    chart();
+  }, []);
+  
 
   return (
     <div className="App">
-      <BarChart chartData={userData}/>
+      <LineChart chartData={userData}/>
     </div>);
 }
 
--- webcan/webcan.py
+++ webcan/webcan.py
@@ -1,7 +1,7 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, jsonify, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -9,19 +9,53 @@ from webcan.db import get_db
 
 bp = Blueprint('graph', __name__, url_prefix='/graph')
 
-@bp.route('/line')
-def register():
-    data = [
-        ("01-01-2022", 10),
-        ("02-01-2022", 20),
-        ("03-01-2022", 30),
-        ("04-01-2022", 40),
-        ("05-01-2022", 50),
-    ]
+@bp.route('/line', methods=['POST'])
+def processJson():
 
-    labels = [row[0] for row in data]
-    values = [row[1] for row in data]
+    data = request.get_json()
+
+    name = data['name']
+    location = data['location']
+    randomlist = data['randomlist']
+
+    return jsonify({'result' : 'Success', 
+                    'name' : name,
+                    'location' : location,
+                    'randomkeyinlist' :randomlist[1]})
 
     
 
-    return render_template('graph.html', labels=labels, values=values)
+@bp.route('/getData', methods=['GET'])
+def getGraphData():
+    UserData = [
+  {
+    'id': 1,
+    'year': 2016,
+    'userGain': 80000,
+    'userLost': 823,
+  },
+  {
+    'id': 2,
+    'year': 2017,
+    'userGain': 45677,
+    'userLost': 345,
+  },
+  {
+    'id': 3,
+    'year': 2018,
+    'userGain': 78888,
+    'userLost': 555,
+  },
+  {
+    'id': 4,
+    'year': 2019,
+    'userGain': 90000,
+    'userLost': 4555,
+  },
+  {
+    'id': 5,
+    'year': 2020,
+    'userGain': 4300,
+    'userLost': 234,
+  }]
+    return jsonify(UserData)
